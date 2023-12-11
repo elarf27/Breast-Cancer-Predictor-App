3d4f47dfe882adee7962eb7b342e30a1b81b6f8d
elarf27 <153523957+elarf27@users.noreply.github.com>
2023-12-11 16:23:32 +0100

    Update main.py

--- model/main.py
+++ model/main.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score, classification_report
 
 
 def get_data():
-    data = pd.read_csv('/Users/abdou/Desktop/VsCode/project/data/data.csv')
+    data = pd.read_csv('data/data.csv')
     data = data.drop(['Unnamed: 32', 'id'], axis = 1)
     data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
 
